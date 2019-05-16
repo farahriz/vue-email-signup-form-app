cbb23459a8511c132d9350685ca8fa7a7bda0009
Farah <41633573+farahriz@users.noreply.github.com>
2019-05-16 12:56:18 +0800

    Write test for form submit event

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,3 +1,15 @@
 <template>
-  <form />
+  <form @submit="onSubmit">
+    <button />
+  </form>
 </template>
+
+<script>
+export default {
+  methods: {
+    onSubmit () {
+      this.$emit('form-submitted')
+    }
+  }
+}
+</script>
